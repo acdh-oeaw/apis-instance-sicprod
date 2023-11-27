ea1d5957a57932ced266bbffa35e11277e14dfb4
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-11-27 11:50:02 +0100

    chore(deps): bump apis-core to v0.7.1
    this fixes the auth.urls include, therefore we don't have to include it
    ourselves anymore

--- sicprod/urls.py
+++ sicprod/urls.py
@@ -1,5 +1,5 @@
 from apis_acdhch_default_settings.urls import urlpatterns
-from django.urls import include, path
+from django.urls import path
 from apis_ontology.views import CustomReferenceDetailView, TempTripleAutocomplete, TempEntityClassAutocomplete, CustomReferenceDeleteView
 from django.contrib.auth.decorators import login_required
 
@@ -8,6 +8,5 @@ customurlpatterns = [
     path('bibsonomy/tempentityclass-autocomplete/', TempEntityClassAutocomplete.as_view(), name="tempentityclass-autocomplete",),
     path('bibsonomy/temptriple-autocomplete/', TempTripleAutocomplete.as_view(), name="temptriple-autocomplete",),
     path('bibsonomy/references/<int:pk>/delete', login_required(CustomReferenceDeleteView.as_view()), name='referencedelete'),
-    path("accounts/", include("django.contrib.auth.urls")),
 ]
 urlpatterns = customurlpatterns + urlpatterns
