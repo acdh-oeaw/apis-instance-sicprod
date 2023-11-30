d31bca2a29d67d8c54973bd3a822f17fbc32079e
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-11-30 09:01:03 +0100

    fix: move apis_ontology app to begin of INSTALLED_APPS list

--- sicprod/settings.py
+++ sicprod/settings.py
@@ -53,6 +53,8 @@ LANGUAGE_CODE = "de"
 
 INSTALLED_APPS += ["apis_bibsonomy"]
 INSTALLED_APPS += ["matomo"]
+INSTALLED_APPS.remove("apis_ontology")
+INSTALLED_APPS = ["apis_ontology"] + INSTALLED_APPS
 MATOMO_URL = "https://matomo.acdh.oeaw.ac.at/"
 MATOMO_SITE_ID = 242
 
