0e4881f783ef38bb24992b828c92d2e116cf3beb
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-23 15:47:27 +0100

    chore(deps): bump apis-core to v0.10.1

--- apis_ontology/views.py
+++ apis_ontology/views.py
@@ -1,23 +1,15 @@
 from apis_bibsonomy.views import ReferenceDetailView, ReferenceDeleteView
 from django.contrib.contenttypes.models import ContentType
-from apis_core.apis_entities.models import TempEntityClass
 from apis_core.apis_relations.models import TempTriple
 from apis_bibsonomy.models import Reference
 from django.shortcuts import redirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django import forms
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView, SingleObjectMixin
-from django.views.generic.edit import FormMixin, FormView, View, DeleteView
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import FormView
 from dal import autocomplete
 from django.db.models import Q
 
-class TempEntityClassAutocomplete(autocomplete.Select2QuerySetView):
-    def get_queryset(self):
-        qs = TempEntityClass.objects.all()
-        if self.q:
-            qs = qs.filter(name__icontains=self.q)
-        return qs
 
 class TempTripleAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
@@ -27,16 +19,18 @@ class TempTripleAutocomplete(autocomplete.Select2QuerySetView):
             qs = qs.filter(q)
         return qs
 
-class TempEntityClassReferenceForm(forms.Form):
-    ReferenceToObject = forms.ModelChoiceField(TempEntityClass.objects.all(),widget=autocomplete.ModelSelect2(url='tempentityclass-autocomplete', attrs={'class': 'form-control'}), label="Add entity")
 
 class TempTripleReferenceForm(forms.Form):
-    ReferenceToObject = forms.ModelChoiceField(TempTriple.objects.all(),widget=autocomplete.ModelSelect2(url='temptriple-autocomplete', attrs={'class': 'form-control'}), label="Add relation")
+    ReferenceToObject = forms.ModelChoiceField(
+            TempTriple.objects.all(),
+            widget=autocomplete.ModelSelect2(url='temptriple-autocomplete', attrs={'class': 'form-control'}),
+            label="Add relation"
+    )
+
 
 class CustomReferenceDetailView(ReferenceDetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tempentityclassform'] = TempEntityClassReferenceForm(prefix="tempentity")
         context['temptripleform'] = TempTripleReferenceForm(prefix="temptriple")
         return context
 
@@ -44,9 +38,9 @@ class CustomReferenceDetailView(ReferenceDetailView):
         view = ReferenceFormView.as_view()
         return view(request, *args, **kwargs)
 
+
 class ReferenceFormView(FormView, SingleObjectMixin):
     model = Reference
-    form_class = TempEntityClassReferenceForm
 
     def get_success_url(self):
         return reverse('referencedetail', kwargs=self.request.resolver_match.kwargs)
@@ -56,14 +50,7 @@ class ReferenceFormView(FormView, SingleObjectMixin):
         newref = self.get_object()
         newref.pk = None
         newref._state.adding = True
-        if 'tempentity-ReferenceToObject' in request.POST:
-            form = TempEntityClassReferenceForm(request.POST, prefix="tempentity")
-            if form.is_valid():
-                rto = form.cleaned_data['ReferenceToObject']
-                newref.content_type = rto.self_contenttype
-                newref.object_id = rto.id
-                newref.save()
-        elif 'temptriple-ReferenceToObject' in request.POST:
+        if 'temptriple-ReferenceToObject' in request.POST:
             form = TempTripleReferenceForm(request.POST, prefix="temptriple")
             if form.is_valid():
                 rto = form.cleaned_data['ReferenceToObject']
@@ -74,6 +61,7 @@ class ReferenceFormView(FormView, SingleObjectMixin):
         else:
             print("form is not valid")
         return redirect(self.get_success_url())
+
 
 class CustomReferenceDeleteView(ReferenceDeleteView):
     def delete(self, request, *args, **kwargs):
--- sicprod/urls.py
+++ sicprod/urls.py
@@ -1,11 +1,10 @@
 from apis_acdhch_default_settings.urls import urlpatterns
 from django.urls import path, include
-from apis_ontology.views import CustomReferenceDetailView, TempTripleAutocomplete, TempEntityClassAutocomplete, CustomReferenceDeleteView
+from apis_ontology.views import CustomReferenceDetailView, TempTripleAutocomplete, CustomReferenceDeleteView
 from django.contrib.auth.decorators import login_required
 
 customurlpatterns = [
     path("bibsonomy/references/<int:pk>", login_required(CustomReferenceDetailView.as_view()), name='referencedetail'),
-    path('bibsonomy/tempentityclass-autocomplete/', TempEntityClassAutocomplete.as_view(), name="tempentityclass-autocomplete",),
     path('bibsonomy/temptriple-autocomplete/', TempTripleAutocomplete.as_view(), name="temptriple-autocomplete",),
     path('bibsonomy/references/<int:pk>/delete', login_required(CustomReferenceDeleteView.as_view()), name='referencedelete'),
 ]
