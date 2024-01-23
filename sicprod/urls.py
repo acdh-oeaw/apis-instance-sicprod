bc5dbf249feeb77c056d65f1d40f9af5e93c2cd2
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-01-23 15:47:27 +0100

    chore(deps): update bibsonomy and drop bibsonomy workarounds
    updated bibsonomy to 0.8.1 which allows deleting using htmx
    a lot of the custom functionality depended on tempentity class, so we
    remove it- it should be redone in bibsonomy anyway

--- sicprod/urls.py
+++ sicprod/urls.py
@@ -1,13 +1,4 @@
 from apis_acdhch_default_settings.urls import urlpatterns
 from django.urls import path, include
-from apis_ontology.views import CustomReferenceDetailView, TempTripleAutocomplete, TempEntityClassAutocomplete, CustomReferenceDeleteView
-from django.contrib.auth.decorators import login_required
 
-customurlpatterns = [
-    path("bibsonomy/references/<int:pk>", login_required(CustomReferenceDetailView.as_view()), name='referencedetail'),
-    path('bibsonomy/tempentityclass-autocomplete/', TempEntityClassAutocomplete.as_view(), name="tempentityclass-autocomplete",),
-    path('bibsonomy/temptriple-autocomplete/', TempTripleAutocomplete.as_view(), name="temptriple-autocomplete",),
-    path('bibsonomy/references/<int:pk>/delete', login_required(CustomReferenceDeleteView.as_view()), name='referencedelete'),
-]
 urlpatterns += [path("", include("django_acdhch_functions.urls")),]
-urlpatterns = customurlpatterns + urlpatterns
