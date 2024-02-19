c437772db5934789fa574856000ec361274b728a
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-19 15:12:29 +0100

    fix(forms): set the initial collection value for all entity types

--- apis_ontology/forms.py
+++ apis_ontology/forms.py
@@ -2,22 +2,24 @@ from apis_core.generic.forms import GenericModelForm
 from apis_ontology.models import Person
 
 
-class PersonForm(GenericModelForm):
+class LegacyStuffMixinForm(GenericModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["collection"].initial = [1, 4]
+
+
+class PersonForm(LegacyStuffMixinForm):
     field_order = ["first_name", "name", "start_date_written", "end_date_written", "status", "collection", "gender"]
 
     class Meta(GenericModelForm.Meta):
         model = Person
         exclude = ["published"]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-        self.fields["collection"].initial = [1,4]
-
-
-class PlaceForm(GenericModelForm):
+class PlaceForm(LegacyStuffMixinForm):
     field_order = ["name", "type", "latitude", "longitude", "status", "collection"]
 
 
-class InstitutionForm(GenericModelForm):
+class InstitutionForm(LegacyStuffMixinForm):
     field_order = ["name", "start_date_written", "end_date_written", "kind", "status", "collection"]
