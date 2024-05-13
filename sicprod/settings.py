0d0ea9a5a04edd9fe3dedf3cfc50ba1053b8a2fe
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-05-13 12:38:34 +0200

    feat(settings): use custom schema generator for spectacular

--- sicprod/settings.py
+++ sicprod/settings.py
@@ -104,3 +104,5 @@ APIS_ENTITIES = {
 INSTALLED_APPS += ["corsheaders"]
 MIDDLEWARE = ["corsheaders.middleware.CorsMiddleware"] + MIDDLEWARE
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://sicprod-frontend.acdh-ch-dev.oeaw.ac.at"]
+
+SPECTACULAR_SETTINGS["DEFAULT_GENERATOR_CLASS"] = 'apis_core.generic.generators.CustomSchemaGenerator'
