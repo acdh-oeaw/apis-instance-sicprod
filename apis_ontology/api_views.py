e858380e62a7026179b82c0abc259290b069e5e9
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-05-28 14:11:39 +0200

    feat(api): override the generic ModelViewSet to add facet information

--- apis_ontology/api_views.py
+++ apis_ontology/api_views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView
 from apis_ontology.serializers import TempTripleSerializer
 from apis_core.apis_relations.models import TempTriple
+from apis_core.generic.api_views import ModelViewSet
 
 
 class ListEntityRelations(ListAPIView):
@@ -20,3 +21,27 @@ class ListEntityRelations(ListAPIView):
         context = super().get_serializer_context()
         context["obj"] = get_object_or_404(contenttype.model_class(), pk=pk)
         return context
+
+
+class SicprodModelViewSet(ModelViewSet):
+    """
+    We override the generic ModelViewSet so we can add the facet
+    information to the -list output
+    """
+    facet_attributes = ["gender", "type"]
+
+    def generate_facet_data(self):
+        facets = {}
+        for el in self.filter_queryset(self.get_queryset()):
+            for attribute in self.facet_attributes:
+                val = getattr(el, attribute, None)
+                if val is not None:
+                    facets[attribute] = facets.get(attribute, {})
+                    facets[attribute][val] = facets[attribute].get(val, 0) + 1
+        return {"facets": facets}
+
+    def list(self, request, *args, **kwargs):
+        res = super().list(request, *args, **kwargs)
+        res.data.update(self.generate_facet_data())
+        res.data = dict(sorted(res.data.items()))
+        return res
--- sicprod/urls.py
+++ sicprod/urls.py
@@ -1,9 +1,16 @@
 from apis_acdhch_default_settings.urls import urlpatterns
 from django.urls import path, include
+from rest_framework import routers
 
-from apis_ontology.api_views import ListEntityRelations
+from apis_ontology.api_views import ListEntityRelations, SicprodModelViewSet
 
 urlpatterns += [path("", include("django_acdhch_functions.urls")),]
 urlpatterns += [path("logger/", include("django_action_logger.urls")),]
 
 urlpatterns += [path("apis/api/<contenttype:contenttype>/<int:pk>/relations", ListEntityRelations.as_view(), name="relationslist")]
+
+# We override the generic ModelViewSet URIs using a custom ModelViewSet
+# so we can add the faceting information to the `list` response
+router = routers.DefaultRouter()
+router.register(r"", SicprodModelViewSet, basename="genericmodelapi")
+urlpatterns.insert(0, path("apis/api/<contenttype:contenttype>/", include(router.urls)))
