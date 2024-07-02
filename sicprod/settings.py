a080c89ff587fadc1247a0deda332760e664bae8
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-07-02 11:12:27 +0200

    chore(deps): remove django_action_logger from project

--- sicprod/settings.py
+++ sicprod/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS += ["apis_bibsonomy"]
 INSTALLED_APPS.remove("apis_ontology")
 INSTALLED_APPS = ["apis_ontology"] + INSTALLED_APPS
 INSTALLED_APPS = ["django_acdhch_functions"] + INSTALLED_APPS
-INSTALLED_APPS += ["django_action_logger"]
 INSTALLED_APPS += ["apis_core.collections"]
 INSTALLED_APPS += ["apis_core.history"]
 INSTALLED_APPS += ["simple_history"]
