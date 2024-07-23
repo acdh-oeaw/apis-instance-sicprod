106e1476b3a0b79a2a8ba2c95410eda8706135ef
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-07-23 11:41:49 +0200

    feat(api): use pagination and use all other entities for facets

--- apis_ontology/api_views.py
+++ apis_ontology/api_views.py
@@ -1,9 +1,69 @@
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView
+from rest_framework import pagination
 from apis_ontology.serializers import TempTripleSerializer
 from apis_core.apis_relations.models import TempTriple
 from apis_core.generic.api_views import ModelViewSet
+from django.db.models import Count
+from django.contrib.contenttypes.models import ContentType
+from apis_core.utils.helpers import get_content_types_with_allowed_relation_from
+import time
+
+
+class InjectFacetPagination(pagination.LimitOffsetPagination):
+    def paginate_queryset(self, queryset, request, view=None):
+        start = time.time()
+        self.facets = self.calculate_facets(queryset)
+        print("Used " + str(time.time() - start) + "ms for calculating the facets")
+        return super().paginate_queryset(queryset, request, view)
+
+    def get_paginated_response(self, data):
+        response = super().get_paginated_response(data)
+        response.data.update(self.facets)
+        response.data = dict(sorted(response.data.items()))
+        return response
+
+    def calculate_facets(self, queryset):
+        facets = {"start": None,
+                  "end": None,
+                  "gender": {},
+                  "type": {}}
+        print("calculate_facets")
+        for obj in queryset:
+            for attribute in ["gender", "type"]:
+                if hasattr(obj, attribute):
+                    value = getattr(obj, attribute)
+                    id = value or "emtpy"
+                    facetdict = facets[attribute].get(id, {"name": value, "count": 0})
+                    facetdict["count"] += 1
+                    facets[attribute][id] = facetdict
+                else:
+                    facets.pop(attribute, None)
+            if start_date := getattr(obj, "start_date", None):
+                facets["start"] = min(start_date.year, facets["start"], key=lambda x: x or 1600)
+            if end_date := getattr(obj, "end_date", None):
+                facets["end"] = max(end_date.year, facets["end"], key=lambda x: x or 1300)
+
+        content_type = ContentType.objects.get_for_model(queryset.model)
+        relation_content_types = get_content_types_with_allowed_relation_from(content_type)
+
+        for relation_content_type in relation_content_types:
+            facetname = "relation_" + relation_content_type.name
+            facets[facetname] = {}
+            model = relation_content_type.model_class()
+            res = list(model.objects.filter(triple_set_from_subj__obj__in=queryset).distinct().annotate(count=Count("id")))
+            res += list(model.objects.filter(triple_set_from_obj__subj__in=queryset).distinct().annotate(count=Count("id")))
+            for obj in res:
+                name = obj.name
+                if relation_content_type.name == "person":
+                    name = f"{obj.first_name} {obj.name}"
+                if obj.id in facets[facetname].keys():
+                    facets[facetname][obj.id]["count"] += obj.count
+                else:
+                    facets[facetname][obj.id] = {"name": name, "count": obj.count}
+        print("end calculate facets")
+        return {"facets": facets}
 
 
 class ListEntityRelations(ListAPIView):
@@ -28,42 +88,5 @@ class SicprodModelViewSet(ModelViewSet):
     We override the generic ModelViewSet so we can add the facet
     information to the -list output
     """
-    facet_attributes = ["gender", "type", "related_persons", "related_functions", "related_places", "related_institutions", "related_events", "related_salaries"]
 
-    def generate_facet_data(self):
-        facets = {"start": None, "end": None}
-        for el in self.filter_queryset(self.get_queryset()):
-            for attribute in self.facet_attributes:
-                values = getattr(el, attribute, [])
-                if isinstance(values, str):
-                    values = [values]
-                if values == None:
-                    values = []
-
-                for value in values:
-                    facets[attribute] = facets.get(attribute, {})
-                    match value:
-                        case dict():
-                            facetdict = facets[attribute].get(value["id"], {"name": value["name"], "count": 0 })
-                            facetdict["count"] += 1
-                            facets[attribute][value["id"]] = facetdict
-                        case str():
-                            id = value or "emtpy"
-                            facetdict = facets[attribute].get(id, {"name": value, "count": 0})
-                            facetdict["count"] += 1
-                            facets[attribute][id] = facetdict
-                        case None:
-                            pass
-                        case other:
-                            print(f"Unusable value for facetlist: {other}")
-            if start_date := getattr(el, "start_date", None):
-                facets["start"] = min(start_date.year, facets["start"], key=lambda x: x or 1600)
-            if end_date := getattr(el, "end_date", None):
-                facets["end"] = max(end_date.year, facets["end"], key=lambda x: x or 1300)
-        return {"facets": facets}
-
-    def list(self, request, *args, **kwargs):
-        res = super().list(request, *args, **kwargs)
-        res.data.update(self.generate_facet_data())
-        res.data = dict(sorted(res.data.items()))
-        return res
+    pagination_class = InjectFacetPagination
