7a1ae3fa432b5cdd4eb690ea1fddca56d31c34e8
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-08-26 08:57:21 +0200

    fix(serializer): make reference file detection more failure resistant

--- apis_ontology/views.py
+++ apis_ontology/views.py
@@ -1,10 +1,8 @@
-import requests
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from auditlog.models import LogEntry
 from apis_bibsonomy.models import Reference
-from apis_ontology.serializers import iiif_titles, get_folio
-from functools import cache
+from apis_ontology.serializers import iiif_titles, get_folio, normalize_title
 import django_tables2 as tables
 from django.utils.html import format_html
 
@@ -16,32 +14,18 @@ class UserAuditLog(LoginRequiredMixin, ListView):
 
 def scanfolderexists(ref):
     if "title" in ref.bibtexjson:
-        normtitle = ref.bibtexjson["title"].replace(" ", "_").replace("(", "").replace(")", "")
-        return normtitle in iiif_titles()
+        return normalize_title(ref.bibtexjson["title"]) in iiif_titles()
     return False
 
 
-@cache
-def iiif_files(title):
-    files = requests.get(f"https://iiif.acdh-dev.oeaw.ac.at/images/sicprod/{title}/", headers={"Accept": "application/json"})
-    return files.json()
-
-
-def scanfileexists(ref):
-    normtitle = ref.bibtexjson["title"].replace(" ", "_").replace("(", "").replace(")", "")
-    folio = get_folio(ref)
-    return f"{folio}.jpg" in iiif_files(normtitle)
-
-
-def scanfile(ref):
-    normtitle = ref.bibtexjson["title"].replace(" ", "_").replace("(", "").replace(")", "")
-    folio = get_folio(ref)
-    return f"<a href='https://iiif.acdh-dev.oeaw.ac.at/images/sicprod/{normtitle}/{folio}.jpg'>{normtitle}/{folio}</a>"
+def scanfolder(ref):
+    normtitle = normalize_title(ref.bibtexjson["title"])
+    return f"<a href='https://iiif.acdh-dev.oeaw.ac.at/images/sicprod/{normtitle}/'>{normtitle}/</a>"
 
 
 class ReferenceFailTable(tables.Table):
     ref = tables.Column(empty_values=())
-    scanfile = tables.Column(empty_values=())
+    folder = tables.Column(empty_values=())
     on = tables.Column(empty_values=())
 
     def render_on(self, record):
@@ -55,8 +39,8 @@ class ReferenceFailTable(tables.Table):
     def render_ref(self, record):
         return str(record)
 
-    def render_scanfile(self, record):
-        return format_html(scanfile(record))
+    def render_folder(self, record):
+        return format_html(scanfolder(record))
 
 
 class ReferenceScanFail(LoginRequiredMixin, tables.SingleTableView):
@@ -66,5 +50,5 @@ class ReferenceScanFail(LoginRequiredMixin, tables.SingleTableView):
     def get_queryset(self, *args, **kwargs):
         refs = Reference.objects.all()
         refs = [ref for ref in refs if scanfolderexists(ref)]
-        refs = [ref for ref in refs if not scanfileexists(ref)]
+        refs = [ref for ref in refs if get_folio(ref) is None]
         return refs
