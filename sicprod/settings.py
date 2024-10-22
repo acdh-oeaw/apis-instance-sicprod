54c3cab762d96287070e56cd3c176f5d2deb8926
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-10-22 13:49:39 +0200

    fix(settings): drop functions from the INSTALLED_APPS

--- sicprod/settings.py
+++ sicprod/settings.py
@@ -23,7 +23,6 @@ LANGUAGE_CODE = "de"
 INSTALLED_APPS += ["apis_bibsonomy"]
 INSTALLED_APPS.remove("apis_ontology")
 INSTALLED_APPS = ["apis_ontology"] + INSTALLED_APPS
-INSTALLED_APPS = ["django_acdhch_functions"] + INSTALLED_APPS
 INSTALLED_APPS += ["apis_core.collections"]
 INSTALLED_APPS += ["apis_core.history"]
 INSTALLED_APPS += ["simple_history"]
