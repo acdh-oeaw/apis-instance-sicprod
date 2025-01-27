2369975436214e7ac53c216581db8a42dc35da28
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-01-27 09:25:13 +0100

    refactor(forms): drop custom collection form field
    apis-core-rdf includes a collection form field in the generic model form
    starting with version 0.36.0.

--- apis_ontology/forms.py
+++ apis_ontology/forms.py
@@ -1,44 +1,22 @@
 from apis_core.generic.forms import GenericModelForm
 from apis_ontology.models import Person
-from apis_core.collections.models import SkosCollection, SkosCollectionContentObject
-from django import forms
-from django.contrib.contenttypes.models import ContentType
 
-
-class MyModelMultipleChoiceField(forms.ModelMultipleChoiceField):
-    pass
 
 from apis_core.generic.forms.widgets import NewlineSeparatedListWidget
 
 
 class SicprodMixinForm(GenericModelForm):
-    collections = MyModelMultipleChoiceField(queryset=SkosCollection.objects.filter(parent=SkosCollection.objects.get(name="sicprod")), required=False)
 
     class Meta(GenericModelForm.Meta):
         exclude = ["deprecated_name"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.parent = SkosCollection.objects.get(name="sicprod")
-        if instance := kwargs.get("instance"):
-            ct = ContentType.objects.get_for_model(instance)
-            sccos = SkosCollectionContentObject.objects.filter(collection__parent=self.parent, content_type=ct, object_id=instance.pk).values_list("collection")
-            self.fields["collections"].initial = SkosCollection.objects.filter(id__in=sccos)
-        else:
+        if not self.fields["collections"].initial:
             self.fields["collections"].initial = [2, 5]
 
-        #self.fields["collection"].initial = [1, 4]
         if "alternative_label" in self.fields:
             self.fields["alternative_label"].widget = NewlineSeparatedListWidget(attrs={"class": "mb-1"})
-
-    def save(self, *args, **kwargs):
-        obj = super().save(*args, **kwargs)
-        ct = ContentType.objects.get_for_model(obj)
-        collections = SkosCollection.objects.filter(parent=self.parent)
-        for skoscollection in self.cleaned_data["collections"]:
-            scco, _ = SkosCollectionContentObject.objects.get_or_create(collection=skoscollection, content_type=ct, object_id=obj.pk)
-        SkosCollectionContentObject.objects.filter(collection__in=collections, content_type=ct, object_id=obj.pk,).exclude(collection__in=self.cleaned_data["collections"]).delete()
-        return obj
 
 
 class PersonForm(SicprodMixinForm):
