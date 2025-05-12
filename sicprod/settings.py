da037cf2df0d1982ebeb326dec67b430348a5878
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-05-12 11:02:41 +0200

    fix(settings): remove apps from INSTALLED_APPS ...
    that are now part of the apis_acdhch_default_settings INSTALLED_APPS

--- sicprod/settings.py
+++ sicprod/settings.py
@@ -20,8 +20,6 @@ LANGUAGE_CODE = "de"
 INSTALLED_APPS += ["apis_bibsonomy"]
 INSTALLED_APPS.remove("apis_ontology")
 INSTALLED_APPS = ["apis_ontology"] + INSTALLED_APPS
-INSTALLED_APPS = ["apis_core.collections"] + INSTALLED_APPS
-INSTALLED_APPS += ["apis_core.history"]
 INSTALLED_APPS += ["simple_history"]
 INSTALLED_APPS += ["auditlog"]
 INSTALLED_APPS += ["django_grouper"]
