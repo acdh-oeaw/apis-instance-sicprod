df8fbd9f8be406e1312203dcb0ef23647d8f46ac
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-08-28 08:52:29 +0200

    fix(settings): drop INSTALLED_APPS that are now part of default settings

--- sicprod/settings.py
+++ sicprod/settings.py
@@ -21,9 +21,7 @@ INSTALLED_APPS += ["apis_bibsonomy"]
 INSTALLED_APPS.remove("apis_ontology")
 INSTALLED_APPS = ["apis_ontology"] + INSTALLED_APPS
 INSTALLED_APPS += ["simple_history"]
-INSTALLED_APPS += ["auditlog"]
 INSTALLED_APPS += ["django_grouper"]
-INSTALLED_APPS += ["apis_acdhch_django_auditlog"]
 INSTALLED_APPS += ["django_interval"]
 PROJECT_METADATA = {
         "matomo_url": "https://matomo.acdh.oeaw.ac.at/",
